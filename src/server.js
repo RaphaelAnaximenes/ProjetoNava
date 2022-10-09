85a8994d50e821e9f9128608510116e766cb85f0
RaphaelAnaximenes <raphaelanaximenesdev@icloud.com>
2022-10-09 20:02:09 -0300

    generator function and readable randomUID

--- src/server.js
+++ src/server.js
@@ -1,32 +1,37 @@
 import { read } from 'fs';
 import http from 'http'
 import {Readable} from 'stream'
+import{randomUUID} from 'crypto'
+
+
+function* run (){
+    for (let index = 0; index <= 50; index++) {
+        const data = {
+            id: randomUUID(),
+            name: `RaphaelRequisition-${index}`
+        }
+
+        yield data
+    }
+}
 
 async function handler(req, res) {
 
-
-    function * run (){
-        
-    }
-
    const readable = new Readable({
         read() {
-        
-        
-        
-        // end data pipeline
-        this.push(null)
+        for(const data of run()){    
+            console.log(`Enviando: `, data)
+            this.push(JSON.stringify(data) + "\n")
         }
-
-    }
-    )
+            this.push(null)
+        }})
 
     readable
         .pipe(res)
         
 
-    res.end('Renderizando! - Server')
-
+    /*res.end('Renderizando! - Server')
+      Não pode usar com pipe, senao fecha a  rendering      */
 }
 
 http.createServer(handler)
